ee5ae3169e74b8dfe260d2366e398f806f3e1cfb
Jeff Nawroth <jeff.nawroth@icloud.com>
2024-02-17 11:04:57 +0100

    - Removed prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
-    "prettier",
   ],
   rules: {
     "vue/multi-word-component-names": "off",
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,9 +6,13 @@
         src="@/assets/logo.svg"
       />
 
-      <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
+      <div class="text-body-2 font-weight-light mb-n1">
+        Welcome to
+      </div>
 
-      <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+      <h1 class="text-h2 font-weight-bold">
+        Vuetify
+      </h1>
 
       <div class="py-14" />
 
